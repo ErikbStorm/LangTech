d881c751f348e7ddac46fedcfe0dae2189aa9935
GuusLake <g.lakeman@student.rug.nl>
2021-05-25 22:46:57 +0200

    Added how many compatibility

--- SparqlAsk copy.py
+++ SparqlAsk copy.py
@@ -8,9 +8,12 @@ nlp = spacy.load("en_core_web_sm")
 def main():
     questions = ['Who are the screenwriters for The Place Beyond The Pines?', 
                 'Who were the composers for Batman Begins?',
-                'What awards did Frozen receive?']
+                'What awards did Frozen receive?',
+                'How many awards did Frozen receive?']
     for question in questions:
+        answer = ask(question, debug=True)
         print(ask(question, debug=True))
+        print(answer)
 
 def execQuery(query, url):
     req = requests.get(url, params={'query': query, 'format': 'json'})
@@ -65,7 +68,10 @@ def getIds(json):
 
 def ask(question, debug=False):
     ent, pred = getEntPred(question)
-    w = question.split()[0].lower()
+    if "how many" in question.lower():
+        w = "how many"
+    else:
+        w = question.split()[0].lower()
     if debug:
         print(w, " | ", ent, " | ", pred)
     if ent == '' or pred == '':
@@ -85,15 +91,18 @@ def getAnswer(w, p_ids, q_ids):
         q_ids = [(q_id, q_label) for q_id, q_label in q_ids if 'date' in q_label.lower().split()]
     elif w == 'where':
         q_ids = [(q_id, q_label) for q_id, q_label in q_ids if 'place' in q_label.lower().split()]
-  
     
     for p_id_list in p_ids:
         p_id = p_id_list[0]
         for q_id_list in q_ids:
             q_id = q_id_list[0]
-                
-            query = "SELECT ?xLabel WHERE {wd:" + p_id + " wdt:" + q_id + " ?x. SERVICE wikibase:label { bd:serviceParam wikibase:language '[AUTO_LANGUAGE],en'. }}"
-            query2 = "SELECT ?xLabel WHERE {?x wdt:" + q_id + " wd:" + p_id + ". SERVICE wikibase:label { bd:serviceParam wikibase:language '[AUTO_LANGUAGE],en'. }}"
+            
+            if (w == "how many"):
+                query = "SELECT (COUNT(?x) as ?count) WHERE {wd:" + p_id + " wdt:" + q_id + " ?x. SERVICE wikibase:label { bd:serviceParam wikibase:language '[AUTO_LANGUAGE],en'. }}"
+                query2 = "SELECT (COUNT(?x) as ?count) WHERE {?x wdt:" + q_id + " wd:" + p_id + ". SERVICE wikibase:label { bd:serviceParam wikibase:language '[AUTO_LANGUAGE],en'. }}"
+            else:
+                query = "SELECT ?xLabel WHERE {wd:" + p_id + " wdt:" + q_id + " ?x. SERVICE wikibase:label { bd:serviceParam wikibase:language '[AUTO_LANGUAGE],en'. }}"
+                query2 = "SELECT ?xLabel WHERE {?x wdt:" + q_id + " wd:" + p_id + ". SERVICE wikibase:label { bd:serviceParam wikibase:language '[AUTO_LANGUAGE],en'. }}"
             #print(query)
             #results = requests.get(url, params={'query': query, 'format': 'json'}).json()
             try:
@@ -110,26 +119,69 @@ def getAnswer(w, p_ids, q_ids):
                 if len(answer) != 0:
                     return answer
                 
+# def getEntPred(question):
+#     parse = nlp(question)    
+#     #displacy.serve(parse, style="ent")
+#     #displacy.serve(parse, style="dep")
+
+#     for token in parse:
+#         print(token.orth_, token.pos_, token.dep_)
+    
+    
+#     ws = ['who', 'what', 'when', 'which', 'where', 'how']
+#     ent = []
+#     pred = []
+
+#     ent = [ent.text for ent in parse.ents][:1]
+    
+#     for word in parse:
+#         if word.dep_ == 'ROOT':
+#             for child in word.children:
+#                 if child.dep_ == 'attr' and child.pos_ == 'NOUN':
+#                     pred = [child.lemma_]
+    
+#     if len(pred) == 0:
+#         pred = [word.orth_ for word in parse if word.dep_ == 'ROOT']
+#     #print(ent, pred)
+#     ent = " ".join(ent)
+#     pred = " ".join(pred)
+#     #print(ent, pred)
+    
+    
+#     return ent, pred
+
 def getEntPred(question):
     parse = nlp(question)    
-    #displacy.serve(parse, style="ent")
-    #displacy.serve(parse, style="dep")
-
-    for token in parse:
-        print(token.orth_, token.pos_, token.dep_)
-    
+    #displacy.render(parse, jupyter=True, style="ent")
+    #displacy.render(parse, jupyter=True, style="dep")
     
     ws = ['who', 'what', 'when', 'which', 'where', 'how']
     ent = []
     pred = []
-
-    ent = [ent.text for ent in parse.ents][:1]
     
     for word in parse:
         if word.dep_ == 'ROOT':
-            for child in word.children:
-                if child.dep_ == 'attr' and child.pos_ == 'NOUN':
-                    pred = [child.lemma_]
+            #print(word.text)
+            for child in word.subtree:
+                if child.dep_ == 'advmod' and child.lemma_.lower() not in ws:
+                    pred = phrase(child)
+                elif child.dep_ == 'nsubj' and child.orth_.lower() not in ws:
+                    ent = phrase(child)
+                    children  = [sub for sub in child.children]
+                    for sub in children:
+                        #print(sub)
+                        if sub.dep_ == 'prep':
+                            #print(sub)
+                            ent = phrase([subsub for subsub in sub.children if subsub.dep_ == 'pobj'][0])
+                            pred = [child.orth_]
+                elif child.dep_ == 'dobj' and child.i < word.i:
+                    pred = phrase(child)
+                elif child.dep_ == 'dobj' and child.i > word.i:
+                    ent = phrase(child)
+                elif child.dep_ == 'agent':
+                    agent = [agent for agent in child.children][0]
+                    #print(agent)
+                    ent = phrase(agent)
     
     if len(pred) == 0:
         pred = [word.orth_ for word in parse if word.dep_ == 'ROOT']
