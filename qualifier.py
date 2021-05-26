732cbc59a114ca9628d682cd80e098877dfd8c22
Ward <ruitenbeek.ward@gmail.com>
2021-05-26 15:06:45 +0200

    added qualifier query, and assignment 4

---
+++ qualifier.py
@@ -0,0 +1,48 @@
+import spacy
+import requests
+
+
+def main():
+    nlp = spacy.load('en_core_web_md')
+    nlp.add_pipe('entityLinker', last=True)
+    question = input('Please ask a question:\n')
+    parse = nlp(question)
+    entities = parse._.linkedEntities
+    verb_dict = {'played': ('actor', 'role'), 'won': ('award', 'winner'), 'released': ('release date', 'publication place')}
+    if len(entities) == 2:
+        for word in parse:
+            if word.dep_ == 'root' and word.lemma_ in verb_dict:
+                prop = verb_dict[word.lemma_]
+    # Get property uri
+    url = 'https://www.wikidata.org/w/api.php'
+    params = {'action': 'wbsearchentities',
+              'language': 'en',
+              'format': 'json',
+              'type': 'property'}
+    prop_ids = list()
+    for p in prop:
+        params['search'] = p
+        json = requests.get(url, params).json()
+        if json['search']:
+            prop_ids.append(json['search'][0]['id'])
+        else:
+            return 'No answer found'
+
+    # create SPARQL query for each key-word
+    answers = list()
+    entity1 = 'Q' + entities[0]
+    entity2 = 'Q' + entities[1]
+    query = "SELECT ?answerLabel WHERE { wd:" + entity2 + " p:" + prop_ids[0] \
+            + " [ ps:" + prop_ids[0] + " ?answer; pq:" + prop_ids[1] + "wd:" + entity1 \
+            + "]. SERVICE wikibase:label { bd:serviceParam wikibase:language 'en' }}"
+    url = 'https://query.wikidata.org/sparql'
+    results = requests.get(url, params={'query': query,
+                                        'format': 'json'}).json()
+    if results['results']['bindings']:
+        for result in results['results']['bindings']:
+            answers.append(result['answerLabel']['value'])
+    return '\n'.join(set(answers))
+
+
+if __name__ == '__main__':
+    main()
